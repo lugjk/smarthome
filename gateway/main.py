fe0db9744c7e8f28ca570de86d27151b1a6c927e
Black-Frost <60770785+Black-Frost@users.noreply.github.com>
2022-04-03 00:26:09 +0700

    Update gateway script
    Add test implementation for serial payload

--- gateway/main.py
+++ gateway/main.py
@@ -10,7 +10,7 @@ ADAFRUIT_SWITCH_FEED_ID = "group-project.bbc-switch"
 ADAFRUIT_BUTTON_FEED_ID = "group-project.bbc-button"
 
 ADAFRUIT_IO_USERNAME = "Frost984"
-ADAFRUIT_IO_KEY = "aio_dIVB65IjbmWjVRvF1jJGuLIvdeJQ"
+ADAFRUIT_IO_KEY = "aio_gyZm62DhiVsTUoEULRrmJQK0ZMyH"
 
 PORT_NAME = "COM4"  #Replace this with the find port function in the book when we don't use the emulator
 
@@ -27,11 +27,22 @@ def disconnected(client):
     print("Disconnected from Adafruit IO")
     sys.exit(1)
 
+def make_payload(id, name, data):
+    return f"!{id}:{name}:{data}#"
 
 def message(client, feed_id, payload):
     print(f"Data received from feed {feed_id}: {payload}")
     #Add an if-else here to compare feed_id and make a proper payload for serial
-    #ser.write((str(payload) + "#").encode())
+    if feed_id == "group-project.bbc-relay":
+        serial_payload = make_payload("11", "RELAY", payload)
+    elif feed_id == "group-project.bbc-buzzer":
+        serial_payload = make_payload("2", "SPEAKER", payload)
+    elif feed_id == "group-project.bbc-led":
+        serial_payload = make_payload("1", "LED", payload)
+    else:
+        return
+    print(f"Serial payload: {serial_payload}")
+    ser.write(serial_payload.encode())
 
 def processData(data):
     print(f"Data from serial: {data}")
