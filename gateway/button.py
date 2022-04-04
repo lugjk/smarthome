5f355b5d34c8e13efe0e4bba3939a7531ea6e2df
Hoa Le <57839244+hoale0231@users.noreply.github.com>
2022-04-04 13:48:19 +0700

    Update button.py

--- gateway/button.py
+++ gateway/button.py
@@ -5,10 +5,14 @@ radio.on_received_string(on_received_string)
 counter_sensor = 0
 radio.set_group(1)
 led.enable(True)
+# basic.show_string(s)
 
 def on_forever():
+    NPNBitKit.button(DigitalPin.P2)
     if NPNBitKit.button(DigitalPin.P2):
-        serial.write_string("!1:BUTTON:" + "1" + "#")
-    else:
-        basic.show_string("!1:BUTTON:" + "0" + "#")
+        s = "!1:BUTTON:" + "1" + "#"
+        serial.write_string(s)
+        basic.pause(100)
+        s = "!1:BUTTON:" + "0" + "#"
+        serial.write_string(s)
 basic.forever(on_forever)
